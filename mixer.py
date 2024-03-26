b3a41fde8b55806b44d8cdf3430c45884420b315
MrM4ngo <thebigbui32@gmail.com>
2024-03-26 08:47:45 -0400

    updated mixer added 021% to percentage bar

--- mixer.py
+++ mixer.py
@@ -3,10 +3,22 @@ from pycaw.pycaw import AudioUtilities, ISimpleAudioVolume
 import tkinter as tk
 from tkinter import StringVar, ttk, DoubleVar
 from random import randint
+from customtkinter import *
+from tkinter import *
+import customtkinter
+from tkinter.ttk import *
+import time
 
-INTERVAL = 205  # 2 seconds
+INTERVAL = 25  # 2 seconds
+
+color = {
+    'grey': '#202020'
+}
+
 
 class WindowsAudioProcess:
+    MxValue = None  # Class variable to hold MxValue
+
     def __init__(self, name, frame) -> None:
         self.name = name
         self.frame = frame
@@ -14,48 +26,66 @@ class WindowsAudioProcess:
         sessions = AudioUtilities.GetAllSessions()
         for session in sessions:
             volume = session._ctl.QueryInterface(ISimpleAudioVolume)
-            if session.Process and session.Process.name() == self.name+".exe":
+            if session.Process and session.Process.name() == self.name + ".exe":
                 self.volume = volume.GetMasterVolume()
 
     def create_panel(self, x: int):
-        name_label = ttk.Label(self.frame, text=self.name, foreground='white', background='black')
-        name_label.grid(row=2, column=x, pady=(10, 5))
+        name = CTkLabel(self.frame, text=self.name.capitalize(), fg_color=color['grey'], bg_color=color['grey'])
+        name.grid(row=2, column=x, pady=(10, 5), padx=(10, 1))
 
         process_volume = DoubleVar(value=self.volume)
-        scale = ttk.Scale(self.frame, from_=100, to=0, orient='vertical', variable=process_volume, command=lambda value: self.set_volume(value))
-        scale.grid(row=3, column=x, pady=(0, 5))
+        scale = CTkSlider(self.frame, from_=0, to=1, orientation='VERTICAL', variable=process_volume,
+                          command=lambda x: self.set_volume(process_volume.get()))
+        scale.grid(row=3, column=x, padx=20, pady=20, sticky="nsew")
+        
+        global formatted_output
+        scale_text = round(self.volume, 2) * 100
+        formatted_output = '{:03d}'.format(int(scale_text)) 
+        if formatted_output <= "0":
+            formatted_output = "0"
+        # scale_text = str(scale_text)
 
-        value_label = ttk.Label(self.frame, text=f"{self.volume:.2f}", foreground="red", font=("Arial", 15))
-        value_label.grid(row=4, column=x)
+        WindowsAudioProcess.MxValue = CTkLabel(self.frame, text=formatted_output+"%", bg_color=color['grey'], font=("Arial", 15))
+        WindowsAudioProcess.MxValue.grid(row=4, column=x)
+        # WindowsAudioProcess.MxValue.pack()
 
-    def set_volume(self, value):
-        volume = float(value) / 100  # Scaling the value to 0-1 range
+    @classmethod
+    def update_value(cls):
+        if cls.MxValue:
+            cls.MxValue.configure(formatted_output+"%") 
+
+            
+
+    def set_volume(self, volume):
         self.volume = volume
         sessions = AudioUtilities.GetAllSessions()
         for session in sessions:
-            volume_interface = session._ctl.QueryInterface(ISimpleAudioVolume)
+            volume = session._ctl.QueryInterface(ISimpleAudioVolume)
             if session.Process and session.Process.name() == self.name + ".exe":
-                volume_interface.SetMasterVolume(self.volume, None)
+                volume.SetMasterVolume(self.volume, None)
+
 
 class VolumeControllerApp:
     def __init__(self) -> None:
         self.processes = []
-        self.root = tk.Tk()
-        self.root.geometry("720x480")
-        self.root.resizable(False, False)
+        self.root = customtkinter.CTk()
+        self.root.geometry("750x450")
+        self.root.resizable(True, True)
         self.root.title("Windows Volume Controller")
 
-        self.mainframe = tk.Frame(self.root, background="black")
-        self.mainframe.pack(fill='both', expand=True, padx=20, pady=20)
+        self.mainframe = CTkFrame(self.root, fg_color=color['grey'])
+        self.mainframe.pack(fill='both', expand=True)
         self.process_names = self.create_process_list()
 
         self.update()
 
         self.root.mainloop()
+        return
 
     def update(self):
+        WindowsAudioProcess.update_value()  # Call update_value from WindowsAudioProcess
         self.create_panel()
-        if self.get_process_names() != self.processes:
+        if self.get_process_names != self.processes:
             self.create_process_list()
         self.root.after(INTERVAL, self.update)
 
@@ -77,5 +107,7 @@ class VolumeControllerApp:
                 process_names.append(session.Process.name())
         return process_names
 
+
 if __name__ == "__main__":
     VolumeControllerApp()
+
---
+++ test.py
@@ -0,0 +1,4 @@
+testFloat = 100
+NewVar = round(testFloat, 2) * 100
+formatted_output = '{:03d}'.format(int(NewVar))
+print(formatted_output)
